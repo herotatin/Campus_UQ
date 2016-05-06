33c743b8672e2921d458ac48d450e3370646f8c3
Jesus Onofre <jesusaog@gmail.com>
2016-05-06 15:03:39 -0500

    Documentado
    Documentado

--- app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/util/CustomTypefaceSpan.java
+++ app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/util/CustomTypefaceSpan.java
@@ -8,27 +8,38 @@ import android.text.style.TypefaceSpan;
 
 
 /**
- * Created by JoseOmar on 1/05/16.
+ * Esta es la clase CustomTypefaceSpan del proyecto campus_UQ de la electiva de moviles
+ * @author: Jose Omar Colorado y Jesus Alberto Onofre
  */
 public class CustomTypefaceSpan extends TypefaceSpan {
-
+    // TypeFace de esta clase
     private final Typeface newType;
-
+    /**
+     * Metodo constructor de la clase CustomTypefaceSpan
+     */
     public CustomTypefaceSpan(String family, Typeface type) {
         super(family);
         newType = type;
     }
-
+    /**
+     * Metodo que hace  que el texto subrayado y en el color de los enlaces.
+     * @ds texto que se subraya
+     */ 
     @Override
     public void updateDrawState(TextPaint ds) {
         applyCustomTypeFace(ds, newType);
     }
-
+    /**
+     * Este metodo no hace nada, ya que ReplacementSpans se miden de forma explícita en lugar de
+     * afectar a las propiedades de la pintura.
+     */ 
     @Override
     public void updateMeasureState(TextPaint paint) {
         applyCustomTypeFace(paint, newType);
     }
-
+    /**
+     * Este metodo cambia la letra
+     */ 
     private static void applyCustomTypeFace(Paint paint, Typeface tf) {
         int oldStyle;
         Typeface old = paint.getTypeface();
