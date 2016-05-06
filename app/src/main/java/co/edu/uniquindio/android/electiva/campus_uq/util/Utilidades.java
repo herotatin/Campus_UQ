a1487c11e0a1dfa71a6660da9239a6c2ba3b6404
Jesus Onofre <jesusaog@gmail.com>
2016-05-06 15:19:14 -0500

    Clase organizada
    Se organizo la clase de utilidades

--- app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/util/Utilidades.java
+++ app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/util/Utilidades.java
@@ -9,18 +9,24 @@ import android.net.NetworkInfo;
 import java.util.Locale;
 
 /**
- * Created by JoseOmar on 26/04/16.
+ * Esta es la clase utilidades del proyecto campus_UQ de la electiva de moviles
+ * @author: Jose Omar Colorado y Jesus Alberto Onofre
  */
 public class Utilidades {
 
-
+    // Variables de la clase utilidades
     public final static String MIS_PREFERENCIAS = "MisPreferencias";
     public final static String LENGUAJE_DE_PREFERENCIA = "languaje_preferences";
     public final static String LENGUAJE_ES = "es";
     public final static String LENGUAJE_EN = "en";
 
 
-
+    /**
+     * Metodo cambiarIdioma 
+     * este es el metodo que se utiliza para realizar el cambio de
+     * idioma de la aplicacion
+     * @param context recibe como parametro un contexto
+     */ 
     public static void cambiarIdioma(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(MIS_PREFERENCIAS, context.MODE_PRIVATE);
         String language = prefs.getString(LENGUAJE_DE_PREFERENCIA, LENGUAJE_ES);
@@ -34,7 +40,12 @@ public class Utilidades {
         editor.commit();
         obtenerLenguaje(context);
     }
-
+    /**
+     * Metodo obtenerLenguaje
+     * Se obtine el lenguaje actual del dispositivo y cambia el de la
+     * app porque se obtiene
+     * @param context Recibe el context actual 
+     */ 
     public static void obtenerLenguaje(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(MIS_PREFERENCIAS, context.MODE_PRIVATE);
         String language = prefs.getString(LENGUAJE_DE_PREFERENCIA, LENGUAJE_ES);
@@ -44,7 +55,11 @@ public class Utilidades {
         config.locale = locale;
         context.getApplicationContext().getResources().updateConfiguration(config, null);
     }
-
+    /**
+     * Metodo verificarConexion
+     * Metodo que se utiliza para verificar la conxion a la red
+     * @param c contexto con la conexion
+     */ 
     public static boolean verificarConexion(Context c){
         ConnectivityManager ConnectionManager=(ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo=ConnectionManager.getActiveNetworkInfo();
