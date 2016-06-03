b4e2ea22959196f8178d9a6c4fc60d95e05b6601
Jesus Onofre <jesusaog@gmail.com>
2016-06-03 09:46:13 -0500

    Fragmen listo
    Documentado

--- app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/fragments/DetalleDeNoticiaFragmentLand.java
+++ app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/fragments/DetalleDeNoticiaFragmentLand.java
@@ -51,7 +51,13 @@ public class DetalleDeNoticiaFragmentLand extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-
+    /**
+     * Metodo onCreate
+     * Es el metodo que se invoca cuando el sistema crea la actividad, se inicializan los
+     * componentes basicos de la actividad.
+     *
+     * @param savedInstanceState informacion actual que se encuentra guardada de la actividad
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +66,10 @@ public class DetalleDeNoticiaFragmentLand extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+    /**
+     * Metodo onCreateView
+     * crea y devuelve la jerarquía vista asociada con el fragmento.
+     */ 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -67,13 +77,23 @@ public class DetalleDeNoticiaFragmentLand extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detalle_de_noticia_fragment_land, container, false);
     }
-
+    
+    
     // TODO: Rename method, update argument and hook method into UI event
+    /**
+     * Metodo onButtonPressed
+     * Metodo en el que se observa el boton presionado por el 
+     * usuario
+     */ 
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
     }
+    /**
+     * Metodo onAttach
+     * llamada una vez que el fragmento se asocia con su actividad
+     */ 
 
     @Override
     public void onAttach(Context context) {
@@ -85,7 +105,10 @@ public class DetalleDeNoticiaFragmentLand extends Fragment {
 //                    + " must implement OnFragmentInteractionListener");
 //        }
     }
-
+    /**
+     * Metodo onDetach
+     * llama inmediatamente antes de la fragmento ya no estar asociado con su actividad
+     */ 
     @Override
     public void onDetach() {
         super.onDetach();
