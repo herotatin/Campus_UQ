1ed1c15e414a73dab6e272f07f116e39548e7b3b
Jesus Onofre <jesusaog@gmail.com>
2016-06-03 10:34:33 -0500

    Update Dependencia.java

--- app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/vo/Dependencia.java
+++ app/src/main/java/co/edu/uniquindio/android/electiva/campus_uq/vo/Dependencia.java
@@ -12,7 +12,9 @@ public class Dependencia implements Parcelable{
 
     private String nombre;
     private String ubicacion;
-
+    /**
+     * Metodo constructor de la clase dependencia
+     */ 
     public Dependencia(String ubicacion, String nombre) {
         this.ubicacion = ubicacion;
         this.nombre = nombre;
@@ -22,19 +24,22 @@ public class Dependencia implements Parcelable{
         nombre = in.readString();
         ubicacion = in.readString();
     }
-
+    // Clase estatica Creator de dependencia
     public static final Creator<Dependencia> CREATOR = new Creator<Dependencia>() {
         @Override
         public Dependencia createFromParcel(Parcel in) {
             return new Dependencia(in);
         }
-
+        /**
+         * Metodo newArray
+         * Metodo que nos da una arreglo de las dependencias
+         */ 
         @Override
         public Dependencia[] newArray(int size) {
             return new Dependencia[size];
         }
     };
-
+    //Metodo get de esta clase
     public String getUbicacion() {
         return ubicacion;
     }
@@ -42,12 +47,19 @@ public class Dependencia implements Parcelable{
     public String getNombre() {
         return nombre;
     }
-
+    /**
+     * Metodo describeContents
+     * cada bit representa un tipo de objeto considera que 
+     * tienen potencial importancia especial cuando se calcula.
+     */ 
     @Override
     public int describeContents() {
         return 0;
     }
-
+    /**
+     * Metodo writeToParcel
+     * Acoplar este objeto en un paquete
+     */ 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(nombre);
